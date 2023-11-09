10315f45e08b43eef75602acd1c239581604b9a5
msinta <shegowmo@gmail.com>
2023-11-09 16:14:32 +0100

    added functon for hex

--- lib/print/print_hex.c
+++ lib/print/print_hex.c
@@ -6,11 +6,23 @@
 /*   By: mshegow <mshegow@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/09 16:09:12 by mshegow       #+#    #+#                 */
-/*   Updated: 2023/11/09 16:11:10 by mshegow       ########   odam.nl         */
+/*   Updated: 2023/11/09 16:14:12 by mshegow       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include "libft.h"
+
 int	hex_len(unsigned int num)
 {
+	int		len;
 
+	len = 0;
+	while (num != 0)
+	{
+		num /= 16;
+		len++;
+	}
+	return (len);
 }
+
