e1387fcacc18b64d80655e82cd75f3bc328569f4
Mohamed Shegow <mshegow@f0r2s19.codam.nl>
2023-11-09 14:54:30 +0100

    added in two new files

--- lib/print/print_ptr.c
+++ lib/print/print_ptr.c
@@ -1,25 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   print_nbr.c                                        :+:    :+:            */
+/*   print_ptr.c                                        :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: mshegow <mshegow@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2023/11/09 14:41:37 by mshegow       #+#    #+#                 */
-/*   Updated: 2023/11/09 14:52:41 by mshegow       ########   odam.nl         */
+/*   Created: 2023/11/09 14:53:21 by mshegow       #+#    #+#                 */
+/*   Updated: 2023/11/09 14:53:22 by mshegow       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-int	print_nbr(int num)
-{
-	int		len;
-	char	*numchar;
-
-	len = 0;
-	numchar = ft_itoa(num);
-	len = print_str(numchar);
-	free(numchar);
-	return (len);
-}
