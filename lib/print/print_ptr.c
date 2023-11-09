731fa2b6b30afbddfd1927f3168d63fbd6719c97
msinta <shegowmo@gmail.com>
2023-11-09 16:08:42 +0100

    function completed

--- lib/print/print_ptr.c
+++ lib/print/print_ptr.c
@@ -6,11 +6,17 @@
 /*   By: mshegow <mshegow@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/09 14:53:21 by mshegow       #+#    #+#                 */
-/*   Updated: 2023/11/09 16:04:32 by mshegow       ########   odam.nl         */
+/*   Updated: 2023/11/09 16:08:31 by mshegow       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-.
+#include "libft.h"
+
+/**
+* @notice	This function counts the lenght of a pointer.
+* @param	uintptr_t			The unsigned integer pointer to count lenght.
+* @return	int					The lenght of characters of the pointer.
+*/
 
 int	ptr_len(unsigned int *num)
 {
@@ -25,8 +31,13 @@ int	ptr_len(unsigned int *num)
 	return (len);
 }
 
+/**
+* @notice	This function converts a pointer.
+* @param	uintptr_t			The unsigned integer pointer to print.
+* @return	void				No return value needed now.
+*/
 
-int	put_ptr(unsigned int *num)
+void	put_ptr(unsigned int *num)
 {
 	if (num >= 16)
 	{
@@ -35,9 +46,36 @@ int	put_ptr(unsigned int *num)
 	}
 	else
 	{
-		if(num <= 9)
-			ft_putchar_fd((num + '0'), 1)
+		if (num <= 9)
+			ft_putchar_fd((num + '0'), 1);
 		else
-			ft_putchar_fd((num - 10 + 'a'))
+			ft_putchar_fd((num - 10 + 'a'), 1);
 	}
+}
+
+/**
+* @notice	This function checks the pointer of something, converts it, prints
+*			it and counts its pointer lenght.
+* @param	unsigned_long_long	The unsigned integer pointer to print.
+* @return	int					The lenght of characters of the printed
+*								pointer.
+*/
+
+int	print_ptr(unsigned long long ptr)
+{
+	int	len;
+
+	len = 2;
+	ft_putnbr_fd("0x", 1);
+	if (ptr == 0)
+	{
+		ft_putchar_fd('0', 1);
+		len++;
+	}
+	else
+	{
+		put_ptr(ptr);
+		len += ptr_len(ptr);
+	}
+	return (len);
 }
