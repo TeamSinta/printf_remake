e47b4aa4ff60ee62bf74cf3e1494db07d7a0df3b
Mohamed Shegow <mshegow@f0r2s19.codam.nl>
2023-11-09 15:00:48 +0100

    created print_unsigned

--- lib/print/print_unsigned.c
+++ lib/print/print_unsigned.c
@@ -6,7 +6,30 @@
 /*   By: mshegow <mshegow@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/09 14:54:16 by mshegow       #+#    #+#                 */
-/*   Updated: 2023/11/09 14:54:17 by mshegow       ########   odam.nl         */
+/*   Updated: 2023/11/09 14:59:51 by mshegow       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	print_unsigned(unsigned int i)
+{
+	int		len;
+	char	*numchar;
+
+	len = 0;
+
+	if (i == 0)
+	{
+
+		ft_putchar_fd('0', 1);
+		len++;
+	}
+	else
+	{
+		numchar = ft_unsigned_itoa(i);
+		len += print_str(numchar);
+		free(numchar);
+	}
+	return (len);
+}
